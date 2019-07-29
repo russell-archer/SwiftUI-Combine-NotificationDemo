5e096d9554eef4495dfa281d3e24690bb3bde4a0
russell-archer <russell.archer@mac.com>
2019-07-29 23:50:52 +0100

    Initial Commit

---
+++ SwiftUI-Combine-NotificationDemo/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftUI-Combine-NotificationDemo
+//
+//  Created by Russell Archer on 29/07/2019.
+//  Copyright © 2019 Russell Archer. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
